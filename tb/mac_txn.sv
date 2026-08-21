58ae4626b9cb58b14498f4091aba5c4af498a281
gnawnah <studyaccforwang@gmail.com>
2026-08-21 21:15:22 +1200

    transaction done

--- tb/mac_txn.sv
+++ tb/mac_txn.sv
@@ -1,6 +1,10 @@
 class mac_txn #(parameter WIDTH = 8);
     rand bit [WIDTH-1:0] a;
     rand bit [WIDTH-1:0] b;
-    bit [4*WIDTH-1:0] acc;
+    bit [4*WIDTH-1:0] acc; // this will be filled by the monitor
+
+    function string convert2string();
+        return $sformatf("a=%0d b=%0d acc=%0d",a,b,acc);
+    endfunction
 
 endclass
