289997cf4fb79cfcd4e9bf61318bbe2ce3decc75
gnawnah <studyaccforwang@gmail.com>
2026-08-21 21:34:32 +1200

    driver done

---
+++ tb/mac_driver.sv
@@ -0,0 +1,23 @@
+virtual mac_if.DRV vif;
+
+class mac_driver #(parameter WIDTH=8);
+
+mailbox #(mac_txn #(WIDTH)) mbx;
+
+function new(virtual mac_if.DRV vif, mailbox #(mac_txn #(WIDTH)) mbx);
+    this.vif = vif;
+    this.mbx = mbx;
+endfunction
+
+task run();
+    forever begin
+        mac_txn #(WIDTH) t;
+        mbx.get(t);
+        @(vif.drv_cb);
+        vif.drv_cb.a <= t.a;
+        vif.drv_cb.b <= t.b;
+        vif.drv_cb.valid <= 1'b1;
+    end
+endtask
+
+endclass
